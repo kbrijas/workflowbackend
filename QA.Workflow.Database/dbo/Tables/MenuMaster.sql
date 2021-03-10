6dcdbe085bc18df1b1964eb7eb1a85f5178311c4
Rijas K B <kbrijas@gmail.com>
2021-03-10 23:35:58 +0300

    Initialize Database Project

---
+++ QA.Workflow.Database/dbo/Tables/MenuMaster.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[MenuMaster] (
+    [SeqNo]       INT           IDENTITY (1, 1) NOT NULL,
+    [Menu]        VARCHAR (500) NOT NULL,
+    [ParentSeqNo] INT           NULL,
+    [MenuOrder]   INT           NULL,
+    [Uisref]      VARCHAR (100) NULL,
+    [CreatedOn]   DATETIME      NOT NULL,
+    [CreatedBy]   VARCHAR (100) NOT NULL,
+    [ModifiedOn]  DATETIME      NULL,
+    [ModifiedBy]  VARCHAR (100) NULL,
+    [IsActive]    BIT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([SeqNo] ASC)
+);
+
